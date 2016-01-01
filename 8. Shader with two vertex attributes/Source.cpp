bc258827b60dfa85d91304c9faa513793c96e58b
Anand Dhavle <ananddhavle@gmail.com>
2016-01-01 23:03:01 +0530

    Shader with vertex position and color as attributes

---
+++ 8. Shader with two vertex attributes/Source.cpp
@@ -0,0 +1,99 @@
+#define GLEW_STATIC
+
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <iostream>
+#include "Shader.h"
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL window", nullptr, nullptr);
+
+	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+	if (window == nullptr)
+	{
+		std::cout << "CANNOT CREATE WINDOW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "GLEW NOT INIT PROPERLY" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetKeyCallback(window, key_callback);
+
+	Shader* shaderProgram = new Shader("Shaders/shader.vs", "Shaders/shader.frag");
+
+
+	GLfloat vertices[] = {
+		0.5f,-0.5f,0.0f, 1.0f,0.0f,0.0f,
+		-0.5f,-0.5f,0.0f, 0.0f,1.0f,0.0f,
+		0.0f,0.5f,0.0f, 0.0f,0.0f,1.0f
+	};
+
+	GLuint VBO, VAO;
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GL_FLOAT), (GLvoid*)( 3 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(0);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		shaderProgram->Use();
+
+		GLfloat timValue = glfwGetTime();
+		GLfloat greenValue = (sin(timValue) / 2) + 0.5;
+		GLint vertexColorLocation = glGetUniformLocation(shaderProgram->program, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+		glBindVertexArray(VAO);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	glfwTerminate();
+	return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
